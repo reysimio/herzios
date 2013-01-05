8d80ca353b74ab6e7e2fd087fcae931a6e6508ea
Jose <vivareysimio@gmail.com>
2013-01-05 02:34:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herzios::Application.config.secret_token = '11c72077cc0d8983a0d721f6ce477db557a2e48dc0099d7dfbfa0b066da4f6f24b4deac80c465aed9e4562a60baa92d5ca357ece53b98f0f5cf3e8d88e1f0ae4'
